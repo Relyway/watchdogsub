e31af98be28b2c8d905acf1927b7193bfa9d9340
Alex <hikol2003@mail.ru>
2025-05-27 21:07:31 +0300

    исправлен фаер для джейсона

--- firebase_init.py
+++ firebase_init.py
@@ -1,24 +1,20 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()                         # читаем .env
-
+import json
 import firebase_admin
 from firebase_admin import credentials, db
 
 def init_firebase():
     """Однократная инициализация Firebase Admin SDK."""
     if firebase_admin._apps:
-        return db                     # уже инициализировано
+        return db
 
-    cred_path = os.getenv("FIREBASE_CREDENTIALS")
-    db_url   = os.getenv("FIREBASE_DB_URL")
-    
+    cred_json = os.environ.get("FIREBASE_CREDENTIALS")
+    db_url = os.environ.get("FIREBASE_DB_URL")
 
-    if not cred_path or not db_url:
-        raise RuntimeError("Заполните FIREBASE_CREDENTIALS и FIREBASE_DB_URL в .env")
+    if not cred_json or not db_url:
+        raise RuntimeError("Заполните FIREBASE_CREDENTIALS и FIREBASE_DB_URL в Environment")
 
-    cred = credentials.Certificate(cred_path)
-    
-    firebase_admin.initialize_app(cred, {"databaseURL": db_url})
+    # Чтение JSON из строки
+    cred = credentials.Certificate(json.loads(cred_json))
+    firebase_admin.initialize_app(cred, {'databaseURL': db_url})
     return db
